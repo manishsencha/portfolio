b0a5115025e023e310afcb8d5d4d3acfc94ddc45
Manish Sencha <manishsencha2001@gmail.com>
2026-04-09 11:12:36 +0530

    Update url

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://manishsencha.com/sitemap.xml',
+    sitemap: 'https://manishsencha.dev/sitemap.xml',
   }
 }
